01cb3c742661d7de615841a84f54aaf2456fc903
kohhengyeap <khykoh.ttwoweb@gmail.com>
2023-07-24 16:04:56 +0800

    test1

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,24 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/resume/'
+    : '/'
+    ? '/resume/about'
+    :'/about',
+  transpileDependencies: true,
+  // publicPath: '/kongji/',
+  lintOnSave:false
+});
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
   ],
+  
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
